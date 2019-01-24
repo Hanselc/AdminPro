377920388ce6413d2b9813c3823e0a739fe95ae0
Hansel Chabur <hansel.chabur@outlook.com>
2019-01-23 21:38:02 -0500

    Modules Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
 import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
-
-
 const appRoutes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphs1', component: Graphs1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: PagenotfoundComponent}
